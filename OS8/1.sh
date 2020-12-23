db72b237151620d21ff15765bfb6d2fc8d3ada18
Amir-Vatani <cache --timeout=86400>
2020-12-23 10:08:36 -0800

    OS8

---
+++ OS8/1.sh
@@ -0,0 +1,4 @@
+git config --global user.email "email"
+git config --global user.name "username"
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=86400'
